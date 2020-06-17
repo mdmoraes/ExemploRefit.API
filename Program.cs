4aec76bc93802178e022264311fe705ecea69e96
Marcos Moraes <mdmoraes.mopsus@gmail.com>
2020-06-16 22:02:15 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ExemploRefit.API
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+        try                                         
+            {
+                var cepClient = RestService.For<ICepApiService>("http://www.viacep.com.br");
+                Console.WriteLine("Informe seu cep:");
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações do Cep: " + cepInformado);
+                var address = await cepClient.GetAddressAsync(cepInformado);
+                Console.Write($"\nLogradouro:{address.Logradouro}," +
+                    $"\nBairro: {address.Bairro}," +
+                    $"\nCidade: {address.Localidade}");
+                
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Erro na consulta de cep: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
